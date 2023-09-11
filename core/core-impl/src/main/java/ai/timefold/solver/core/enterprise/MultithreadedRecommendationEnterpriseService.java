bf946979889299a56c9e847940eb974882d4de59
Lukáš Petrovický <lukas@petrovicky.net>
2023-09-11 11:16:49 +0200

    split the impls

---
+++ core/core-impl/src/main/java/ai/timefold/solver/core/enterprise/MultithreadedRecommendationEnterpriseService.java
@@ -0,0 +1,32 @@
+package ai.timefold.solver.core.enterprise;
+
+import java.util.Iterator;
+import java.util.ServiceLoader;
+import java.util.function.Function;
+
+import ai.timefold.solver.core.api.score.Score;
+import ai.timefold.solver.core.impl.score.director.InnerScoreDirector;
+import ai.timefold.solver.core.impl.solver.RecommendationProcessor;
+
+public interface MultithreadedRecommendationEnterpriseService {
+
+    static MultithreadedRecommendationEnterpriseService load(Integer moveThreadCount) {
+        ServiceLoader<MultithreadedRecommendationEnterpriseService> serviceLoader =
+                ServiceLoader.load(MultithreadedRecommendationEnterpriseService.class);
+        Iterator<MultithreadedRecommendationEnterpriseService> iterator = serviceLoader.iterator();
+        if (!iterator.hasNext()) {
+            throw new IllegalStateException(
+                    "Multi-threaded recommendation API requested with moveThreadCount (" + moveThreadCount
+                            + ") but Timefold Solver Enterprise Edition not found on classpath.\n" +
+                            "Either add the ai.timefold.solver.enterprise:timefold-solver-enterprise-core dependency, " +
+                            "or remove moveThreadCount from solver configuration.\n" +
+                            "Note: Timefold Solver Enterprise Edition is a commercial product.");
+        }
+        return iterator.next();
+    }
+
+    <Solution_, In_, Out_, Score_ extends Score<Score_>> RecommendationProcessor<Solution_, Out_, Score_> buildProcessor(
+            int moveThreadCount, InnerScoreDirector<Solution_, Score_> scoreDirector, Function<In_, Out_> valueResultFunction,
+            Score_ originalScore, In_ clonedElement);
+
+}
