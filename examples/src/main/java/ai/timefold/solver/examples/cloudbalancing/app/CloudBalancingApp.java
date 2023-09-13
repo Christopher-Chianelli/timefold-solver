c5f29e1a0e9cdaf2c3bc875bfa24e6f679d16c0a
Lukáš Petrovický <lukas@petrovicky.net>
2023-09-13 09:40:12 +0200

    testing setup

--- examples/src/main/java/ai/timefold/solver/examples/cloudbalancing/app/CloudBalancingApp.java
+++ examples/src/main/java/ai/timefold/solver/examples/cloudbalancing/app/CloudBalancingApp.java
@@ -1,6 +1,15 @@
 package ai.timefold.solver.examples.cloudbalancing.app;
 
+import java.io.File;
+import java.time.Duration;
+
+import ai.timefold.solver.core.api.score.buildin.hardsoft.HardSoftScore;
+import ai.timefold.solver.core.api.solver.SolutionManager;
+import ai.timefold.solver.core.api.solver.Solver;
+import ai.timefold.solver.core.api.solver.SolverFactory;
+import ai.timefold.solver.core.config.solver.SolverConfig;
 import ai.timefold.solver.examples.cloudbalancing.domain.CloudBalance;
+import ai.timefold.solver.examples.cloudbalancing.domain.CloudProcess;
 import ai.timefold.solver.examples.cloudbalancing.persistence.CloudBalanceSolutionFileIO;
 import ai.timefold.solver.examples.cloudbalancing.swingui.CloudBalancingPanel;
 import ai.timefold.solver.examples.common.app.CommonApp;
@@ -16,8 +25,33 @@ public class CloudBalancingApp extends CommonApp<CloudBalance> {
     public static final String DATA_DIR_NAME = "cloudbalancing";
 
     public static void main(String[] args) {
-        prepareSwingEnvironment();
-        new CloudBalancingApp().init();
+        SolverFactory<CloudBalance> solverFactory = SolverFactory.createFromXmlResource(SOLVER_CONFIG);
+        Solver<CloudBalance> solver = solverFactory.buildSolver();
+        CloudBalance solution = solver.solve(new CloudBalanceSolutionFileIO()
+                .read(new File("data/cloudbalancing/unsolved/1600computers-4800processes.json")));
+        CloudProcess process = solution.getProcessList().get(0);
+        process.setComputer(null);
+
+        for (int i = 0; i < 16; i = Math.max(1, i * 2)) {
+            run(solution, process, i);
+        }
+    }
+
+    private static void run(CloudBalance solution, CloudProcess process, int i) {
+        SolverConfig solverConfig = SolverConfig.createFromXmlResource(SOLVER_CONFIG);
+        solverConfig.setMoveThreadCount(i == 0 ? "NONE" : Integer.toString(i));
+        SolverFactory<CloudBalance> factory = SolverFactory.create(solverConfig);
+        SolutionManager<CloudBalance, HardSoftScore> solutionManager = SolutionManager.create(factory);
+        long totalTime = 0;
+        for (int j = 0; j < 1_000; j++) {
+            long startTime = System.nanoTime();
+            var recommendedFitList =
+                    solutionManager.recommendFit(solution, process, CloudProcess::getComputer);
+            long endTime = System.nanoTime() - startTime;
+            totalTime += endTime;
+        }
+        System.out.printf("Move thread count: %2d - Average time: %d millis%n",
+                i, Duration.ofNanos(totalTime / 1_000).toMillis());
     }
 
     public CloudBalancingApp() {
